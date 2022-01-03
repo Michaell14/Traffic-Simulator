cd29c2925d8564374a2e9e1d718da135bf1e79ce
Michael <limichael909@gmail.com>
2022-01-02 23:46:11 -0500

    master

--- public/bundler.js
+++ public/bundler.js
@@ -1,6 +1,7 @@
 // Initialize Cloud Firestore through Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
+import { getFirestore, collection, query, doc, getDocs, setDoc } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCQoAQ0c5BydPMrr_zhYOlv7StJHrlAIA0",
   authDomain: "traffic-simulator-e5f10.firebaseapp.com",
@@ -14,24 +15,47 @@ const firebaseConfig = {
 const app =initializeApp(firebaseConfig);
 const db = getFirestore();
 
+let scores=[]
+const q = query(collection(db, "scores"));
 
-//On Window Close
-/*
-window.onbeforeunload = function (e) {
-  var e = e || window.event;
+//Gets all the high scores from the firestore database
+const querySnapshot = await getDocs(q);
+querySnapshot.forEach((doc) => {
+  scores.push([doc.data().score, doc.id]);
+});
 
-  add();
-  return 'Please press the Logout button to logout.';
-};
+//Sorts the high scores from lowest to highest
+scores = scores.sort(function(a, b) {
+  return a[0] - b[0];
+});
 
-async function add(){
-  try {
-    const docRef = await addDoc(collection(db, "users"), {
-      first: "Alan"
-    });
+//Adds the current score to firestore database when it becomes a high score.
+setInterval(async function() {
+  let time = document.getElementById("timer").innerHTML.split(":");
+  let currScore = parseInt(time[0]) * 3600 + parseInt(time[1]) * 60 + parseInt(time[2]);
   
-    console.log("Document written with ID: ");
-  } catch (e) {
-    console.error("Error adding document: ", e);
+  //Checks if the score is greater than lowest high score
+  if (currScore>scores[0][0]){
+    await setDoc(doc(db, "scores", scores[0][1]), {
+      score: currScore
+    });
   }
-}*/
+
+}, 1000);
+
+for (let i=0; i<scores.length; i++){
+  createScores(scores[i][0]);
+}
+
+//Adds the scores to the web page
+function createScores(score){
+  const newScore=document.createElement("div");
+  const newH2 = document.createElement("h3");
+
+  newScore.className = "score";
+  
+  newH2.innerHTML = score;
+  newScore.append(newH2);
+
+  document.getElementById("scores").append(newScore);
+}
--- public/script.js
+++ public/script.js
@@ -1,20 +1,24 @@
-const vehicles=["bus", "cars/redcar","cars/browncar","cars/greencar","cars/orangecar","cars/purplecar", "police", "truck"]
+//Vehicles options
+const vehicles=["bus.png", "cars/redcar.png","cars/browncar.png","cars/greencar.png","cars/orangecar.png","cars/purplecar.png", "police.png", "truck.gif"]
 
+//Randomly creates the next vehicle in line
 function createVehicle(index){
     if (index<=0){
         return;
     }
+
+    //Creates the vehicle img tag and adds it to the document
     var newVehicle = document.createElement("img");
-    newVehicle.src="assets/"+vehicles[Math.floor(Math.random()*vehicles.length)] + ".png";
+    newVehicle.src="assets/"+vehicles[Math.floor(Math.random()*vehicles.length)];
     newVehicle.className="vehicleright";
     newVehicle.style.top = "85px";
     document.getElementById("car_line").append(newVehicle);
 
     setTimeout(() => {  createVehicle(index-1) }, calcTime());
 }
-setTimeout(() => {  createVehicle(20) }, calcTime());
+setTimeout(() => {  createVehicle(200) }, calcTime());
 
-//Horn Sound
+//Horn Sound when space is pressed
 document.body.onkeydown = function(e){
     if(e.keyCode == 32){
         var audio = new Audio('assets/carhorn.wav');
@@ -22,16 +26,19 @@ document.body.onkeydown = function(e){
     }
 }
 
+//Calculates time needed before next vehicle spawns based on window width
 function calcTime(){
   const speed =window.innerWidth / 30;
   const time = 64/speed* 1000;
   return time;
 }
 
-
+//Moves the vehicle onto the next lane when it reaches the end of its current lane
 setInterval(function() {
+
     var vehiclesright= document.getElementsByClassName("vehicleright");
     var vehiclesleft= document.getElementsByClassName("vehicleleft");
+
     //Checks when the vehicles reach the right
     for (let i=0; i<vehiclesright.length; i++){
         const rect = vehiclesright[i].getBoundingClientRect();
@@ -44,6 +51,7 @@ setInterval(function() {
         }
     }
 
+    //Checks when the vehicles reach the left
     for (let i=0; i<vehiclesleft.length; i++){
         const rect = vehiclesleft[i].getBoundingClientRect();
         if (rect.right<0){
@@ -54,19 +62,43 @@ setInterval(function() {
         }
     }
 
-}, 200);
+}, 50);
 
-//Timer
+//Stopwatch
 T = {} ;
 T.timerDiv = document.getElementById('timer');
 
+//Displays the stopwatch 
 function displayTimer() {
-  // initilized all local variables:
-  var hours='00', minutes='00', seconds='00', time = '', timeNow = new Date().getTime();
 
+  var hours='00', minutes='00', seconds='00', time = '', timeNow = new Date().getTime();
   T.difference = timeNow - T.timerStarted;
 
-  // seconds
+  // Calculates number of hours
+  if(T.difference > 3600000) {
+    hours = Math.floor(T.difference/3600000);
+
+    if(hours < 10) {
+      hours = '0'+String(hours);
+    }
+
+    T.difference=T.difference%3600000;
+  }
+
+
+  // Calculates number of minutes
+  if(T.difference >= 60000) {
+    minutes = Math.floor(T.difference/60000);
+    if (minutes > 60) {
+      minutes = minutes % 60;
+    }
+    if(minutes < 10) {
+      minutes = '0'+String(minutes);
+    }
+    T.difference = T.difference%60000;
+  }
+
+  // Calculates number of seconds
   if(T.difference > 1000) {
     seconds = Math.floor(T.difference / 1000);
     if (seconds > 60) {
@@ -77,31 +109,7 @@ function displayTimer() {
     }
   }
 
-  // minutes
-  if(T.difference > 60000) {
-    minutes = Math.floor(T.difference/60000);
-    if (minutes > 60) {
-      minutes = minutes % 60;
-    }
-    if(minutes < 10) {
-      minutes = '0'+String(minutes);
-    }
-  }
-
-  // hours
-  if(T.difference > 3600000) {
-    hours = Math.floor(T.difference/3600000);
-
-    if(hours < 10) {
-      hours = '0'+String(hours);
-    }
-  }
-
-  time  =  hours   + ':';
-  time += minutes + ':';
-  time += seconds;
-
-  T.timerDiv.innerHTML = time;
+  T.timerDiv.innerHTML = hours   + ':' + minutes + ":" + seconds;
 }
 
 // save start time
@@ -110,8 +118,8 @@ T.timerStarted = new Date().getTime()
 if (T.difference > 0) {
     T.timerStarted = T.timerStarted - T.difference
 }
+
 // update timer periodically
 T.timerInterval = setInterval(function() {
     displayTimer()
-}, 100);
-
+}, 1000);
